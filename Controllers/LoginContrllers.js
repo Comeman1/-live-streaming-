a812c4219f8c1ca96806768e66501013152a467f
17969923438 <1796992438@qq.com>
2018-10-18 14:23:39 +0800

    session

--- Controllers/LoginContrllers.js
+++ Controllers/LoginContrllers.js
@@ -1,6 +1,34 @@
-/**
- * Created by 10143 on 2018/10/17.
- */
+
+exports.index=function(req,res){
+    if(req.session.sign){
+        res.render('index',{state:2});
+        return;
+    }
+
+    var tel =req.cookies.tel;
+    var password=req.cookies.password;
+
+    if(tel==null||password==null){
+        res.render('index',{state:-1});
+    }else{
+        //(1)引入userService
+        var UserService = require('../Service/loginUserService');
+        //(2)创建对象
+        var userService = new UserService();
+        //(3)对象初始化
+        userService.init();
+        //(4)验证用户都合法
+        userService.checkUser(tel,password,function(result){
+            if(result.state==2)
+            {
+                req.session.sign=true;
+                res.render('index',{state:2});
+            }else{
+                res.render('index',{state:-1});
+            }
+        },1);
+    }
+}
 
 exports.login=function(req,res){
     //1,解析客户端提交的数据
@@ -16,6 +44,13 @@ exports.login=function(req,res){
     userService.init();
     //(4)验证用户是否合法
     userService.checkUser(tel,password,function(result){
+        if(result.state==2) {
+            req.session.sign = true;
+            res.cookie('tel', result.tel, {maxAge: 60 * 60 * 1000});
+            res.cookie('password', result.password, {maxAge: 60 * 60 * 1000});
+        }
+        result.tel=null;
+        result.password=null;
         res.end(JSON.stringify(result));
     });
 
